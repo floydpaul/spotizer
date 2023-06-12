4498b87efb1afdbaf9a67eebc13cfd667dc02a03
Paul Floyd <paulbitsindou@outlook.fr>
2023-06-12 09:17:32 +0200

    mise à jour queries.php

--- morceau.php
+++ morceau.php
@@ -27,22 +27,6 @@ require_once('queries.php');
     </div>
     <div class="contener">
         <div class="connexion background2 flex">
-<<<<<<< HEAD
-        <?php 
-            
-            if(!empty($_SESSION)){?>
-                    <a class="a2" href="user_profile.php">Mon profil</a>
-                    <a class="bttn1" href="logout.php">SE DÉCONNECTER</a>
-            <?php
-            }
-            else {?>
-                <a class="a2" href="inscription.php">S'INSCRIRE</a>
-                <a class="bttn1" href="connexion.php">SE CONNECTER</a>
-            <?php
-            }
-        
-        ?>
-=======
             <?php 
             
                 if(!empty($_SESSION)){?>
@@ -56,7 +40,6 @@ require_once('queries.php');
                 }
             
             ?>
->>>>>>> c25792c858ca8ed97884fdb5a3706866994ceb4a
         </div>
         <div class="pd-50 color1">
             <div class="flex">
--- queries.php
+++ queries.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-
 include('bdd.php');
 /* requête pour récupérer tous les morceaux */
 $query = $db->prepare('SELECT * FROM morceau NATURAL JOIN utilisateur');
@@ -32,21 +30,21 @@ if(isset($_POST['contenu'])){
 	$query->execute([
 		'date_publication' => date("Ymd"),
 		'contenu' => $_POST['contenu'],
-		'id_utilisateur' => $_SESSION['id_utilisateur'],
+		'id_utilisateur' => 1,
 		'id_morceau' => $_GET['id_morceau'],
 		]);
 
 	header('location:morceau.php?id_morceau='.$_GET['id_morceau'].'');
 }
 /*requête pour récupérer les informations d'un profil selon son identifiant*/ 
-if (isset $_GET['id_utilisateur']) {
-	$query = $db->prepare('SELECT * FROM utilsateur WHERE id_utilisateur ='.$_GET['id_utilisateur']'');
+if (isset ($_GET['id_utilisateur'])) {
+	$query = $db->prepare('SELECT * FROM utilsateur WHERE id_utilisateur ='.$_GET['id_utilisateur'].'');
 	$query->execute();
 	$utilisateur = $query->fetchAll();
 }
 /*requête pour récuperer les commentaires faits par un utilisateur*/
-if (isset $_GET['id_utilisateur']) {
-	$query = $db->prepare('SELECT * FROM commentaires NATURAL JOIN utilsateur WHERE id_utilisateur ='.$_GET['id_utilisateur']'');
+if (isset ($_GET['id_utilisateur'])) {
+	$query = $db->prepare('SELECT * FROM commentaires NATURAL JOIN morceau WHERE id_utilisateur ='.$_GET['id_utilisateur'].'');
 	$query->execute();
 	$comments = $query->fetchAll();
 }
