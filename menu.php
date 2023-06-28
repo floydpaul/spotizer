61ae617a5e9b7a3a21a7a54b1cc754e8017f4b4e
GregChasseneuil <gherbin@deastanceservices.fr>
2023-06-28 11:24:55 +0200

    Mise à jour Css Responsive

--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <div id="menu">
     <img id="logo" src="icones/logo_spotizer_white.svg">
     <nav>
-        <div class="element"><img class="icon" src="icones/accueil_white.svg" alt=""><a class="a2" href="index.php">Accueil</a></div>
-        <div class="element"><img class="icon" src="icones/categories_white.svg" alt=""><a class="a2" href="categories.php">Catégories</a></div>
-        <div class="element"><img class="icon" src="icones/artiste_white.svg" alt=""><a class="a2" href="membres.php">Membres</a></div>
-        <div class="element"><img class="icon" src="icones/playlist_white.svg" alt=""><a class="a2" href="playlists.php">Playlists</a></div>   
+        <div class="element"><a class="a2" href="index.php"><img class="icon" src="icones/accueil_white.svg" alt=""><span>Accueil</span></a></div>
+        <div class="element"><a class="a2" href="categories.php"><img class="icon" src="icones/categories_white.svg" alt=""><span>Catégories</span></a></div>
+        <div class="element"><a class="a2" href="membres.php"><img class="icon" src="icones/artiste_white.svg" alt=""><span>Membres</span></a></div>
+        <div class="element"><a class="a2" href="playlists.php"><img class="icon" src="icones/playlist_white.svg" alt=""><span>Playlists</span></a></div>   
     </nav>
 </div>
 
